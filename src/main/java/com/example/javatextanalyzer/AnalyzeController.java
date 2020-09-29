0d103e229adb764200e3f753d5e3665e241c0be4
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-30 02:26:11 +0300

    Add character counter

--- src/main/java/com/example/javatextanalyzer/AnalyzeController.java
+++ src/main/java/com/example/javatextanalyzer/AnalyzeController.java
@@ -4,18 +4,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 @RestController
 public class AnalyzeController {
 
     @PostMapping("/analyze")
     public Result result(@RequestBody Request request) {
-        return new Result(getTextLength(request.getText()));
+        return new Result(getTextLength(request.getText()),getWordCount(request.getText()),getCharacterCount(request.getText()));
     }
 
     public TextLength getTextLength(String text) {
         long withSpaces = text.length();
         long withoutSpaces = text.replaceAll(" ","").length();
         return new TextLength(withSpaces,withoutSpaces);
+    }
+
+    public long getWordCount(String text) {
+        if(text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = text.split("\\s+");
+
+        return words.length;
+    }
+
+    public Object[] getCharacterCount(String text) {
+
+        String cleanString = text.toLowerCase().replaceAll("[^a-z]","");
+
+        TreeMap<Character, Integer> charCountMap = new TreeMap<Character, Integer>();
+
+        char[] strArray = cleanString.toCharArray();
+
+        for(char c : strArray) {
+            if(charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1 );
+            }else {
+                charCountMap.put(c, 1);
+            }
+        }
+
+
+
+
+        return charCountMap.entrySet().toArray();
     }
 
 
--- src/main/java/com/example/javatextanalyzer/Result.java
+++ src/main/java/com/example/javatextanalyzer/Result.java
@@ -1,13 +1,28 @@
 package com.example.javatextanalyzer;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Result {
     private TextLength textLength;
+    private long wordCount;
+    private Object[] characterCount;
 
-    public Result(TextLength textLength) {
+    public Result(TextLength textLength, long wordCount, Object[] characterCount) {
         this.textLength = textLength;
+        this.wordCount = wordCount;
+        this.characterCount = characterCount;
+    }
+
+    public Object[] getCharacterCount() {
+        return characterCount;
     }
 
     public TextLength getTextLength() {
         return textLength;
+    }
+
+    public long getWordCount() {
+        return wordCount;
     }
 }
